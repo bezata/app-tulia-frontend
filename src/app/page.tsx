fa641c02f3d0da16c1e5a3e7e4540183f822578c
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2024-03-05 17:18:03 +0300

    Custom Wallet integrated with theme and provider initialized.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1></h1>
       <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </div>
   );

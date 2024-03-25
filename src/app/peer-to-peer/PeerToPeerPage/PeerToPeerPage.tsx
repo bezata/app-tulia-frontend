33f4e53b543083d155fb1278cedb419c612fc2f7
dogan can selek <canselek2434@gmail.com>
2024-03-25 21:33:22 +0300

    Add SectionSelector component and example state management

--- src/app/peer-to-peer/PeerToPeerPage/PeerToPeerPage.tsx
+++ src/app/peer-to-peer/PeerToPeerPage/PeerToPeerPage.tsx
@@ -1,11 +1,26 @@
 'use client';
 import { PeerToPeerTable } from '@/components/PeerToPeerTable/PeerToPeerTable';
 import { columns } from '@/components/PeerToPeerTable/columns';
+import SectionSelector from '@/components/SectionSelector/SectionSelector';
+import { setExample } from '@/lib/features/example/exampleSlice';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import React from 'react';
 
 const PeerToPeerPage = ({ data }: { data: any }) => {
+  const example = useAppSelector(state => state.example.example);
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    dispatch(setExample('ciguli deneme'));
+  }, []);
+
+  React.useEffect(() => {
+    console.log(example);
+  }, [example]);
+
   return (
     <div className="container mx-auto py-10">
+      <SectionSelector />
       <h1 className="text-3xl font-bold mb-5">Loan Request</h1>
       <PeerToPeerTable columns={columns} data={data} />
     </div>

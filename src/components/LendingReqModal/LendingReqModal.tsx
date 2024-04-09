e09b8fbfffadcfbd091cfc9ab41dcf99ccb70561
dcselek <canselek2434@gmail.com>
2024-04-10 02:34:23 +0300

    Refactor LendingReqModal component to add popover functionality and use date-fns for date formatting

--- src/components/LendingReqModal/LendingReqModal.tsx
+++ src/components/LendingReqModal/LendingReqModal.tsx
@@ -9,7 +9,7 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
-import { PlusCircle } from 'lucide-react';
+import { PlusCircle, CalendarDays } from 'lucide-react';
 import { useCreateTuliaPool } from '@/lens/lens';
 import { ComboBoxResponsive } from '../Combobox/Combobox';
 import { useForm } from 'react-hook-form';
@@ -21,6 +21,9 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
 import { Label } from '../ui/label';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { format } from 'date-fns';
+import { Calendar } from '../ui/calendar';
 
 const schema = z.object({
   lendCoin: z.object({
@@ -119,6 +122,12 @@ const LendingReqModal = () => {
                 <span className="absolute right-8 text-gray-500 top-2">
                   {lendCoin?.label}
                 </span>
+                <span className="text-gray-500 text-xs">
+                  {watch('loanAmount') ? watch('loanAmount') : 0}{' '}
+                  {lendCoin?.label} ={' '}
+                  {watch('loanAmount') ? watch('loanAmount') : 0}{' '}
+                  {barrowCoin?.label}
+                </span>
               </div>
               {errors.loanAmount && (
                 <span className="text-red-500">This field is required</span>
@@ -137,7 +146,7 @@ const LendingReqModal = () => {
                     id="compound"
                   />
                   <Label htmlFor="compound">
-                    Compound <span className="text-gray-500">- 5%</span>
+                    Compound <span className="text-red-500">- 5%</span>
                   </Label>
                 </div>
                 <RadioGroupItem
@@ -146,7 +155,7 @@ const LendingReqModal = () => {
                   id="simple"
                 />
                 <Label htmlFor="simple">
-                  Simple <span className="text-gray-500">- 5%</span>
+                  Simple <span className="text-red-500">- 5%</span>
                 </Label>
                 <RadioGroupItem
                   {...register('interestModal')}
@@ -154,26 +163,78 @@ const LendingReqModal = () => {
                   id="marketBased"
                 />
                 <Label htmlFor="marketBased">
-                  Market Based <span className="text-gray-500">- 5%</span>
+                  Market Based <span className="text-red-500">- 5%</span>
                 </Label>
               </RadioGroup>
             </div>
             <div className="flex flex-col gap-1">
               <Label htmlFor="publishDate">Publish Date</Label>
-              <Input
-                type="date"
-                {...register('publishDate', { required: true })}
-              />
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={'outline'}
+                    className={`flex items-center gap-1 ${
+                      errors.publishDate ? 'border-red-500' : ''
+                    }`}
+                  >
+                    {watch('publishDate') ? (
+                      format(watch('publishDate'), 'PP')
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                    <CalendarDays className="ml-auto h-4 w-4 opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={watch('publishDate')}
+                    onSelect={(date: Date | undefined) =>
+                      date && setValue('publishDate', date)
+                    }
+                    disabled={(date: Date) => {
+                      return date < new Date();
+                    }}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
               {errors.publishDate && (
                 <span className="text-red-500">This field is required</span>
               )}
             </div>
             <div className="flex flex-col gap-1">
               <Label htmlFor="endDate">End Date</Label>
-              <Input type="date" {...register('endDate', { required: true })} />
-              {errors.endDate && (
-                <span className="text-red-500">This field is required</span>
-              )}
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={'outline'}
+                    className={`flex items-center gap-1 ${
+                      errors.endDate ? 'border-red-500' : ''
+                    }`}
+                  >
+                    {watch('endDate') ? (
+                      format(watch('endDate'), 'PP')
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                    <CalendarDays className="ml-auto h-4 w-4 opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={watch('endDate')}
+                    onSelect={(date: Date | undefined) =>
+                      date && setValue('endDate', date)
+                    }
+                    disabled={(date: Date) => {
+                      return date < new Date(watch('publishDate'));
+                    }}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
             </div>
           </div>
           <div className="flex justify-end gap-4 mt-4">

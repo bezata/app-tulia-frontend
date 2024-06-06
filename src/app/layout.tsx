054739bf10665f07f289e29e504fa89fff4d4c74
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2024-06-06 23:28:14 +0300

    RainbowWallet completely removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
 import MainLayout from '@/components/MainLayout/MainLayout';
 import { Toaster } from '@/components/ui/sonner';
 import { Providers } from '@/configs/providersConfig';

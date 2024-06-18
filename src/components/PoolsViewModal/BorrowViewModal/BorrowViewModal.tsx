501f3894669fd220e1af5ff298d1957917af023d
Pufyda <juandecrime@gmail.com>
2024-06-19 02:56:50 +0300

    TEST

--- src/components/PoolsViewModal/BorrowViewModal/BorrowViewModal.tsx
+++ src/components/PoolsViewModal/BorrowViewModal/BorrowViewModal.tsx
@@ -110,6 +110,16 @@ const BorrowViewModal = ({ row }: IPoolsViewModalProps) => {
           <div className="col-span-12 flex gap-2 flex-col">
             {row.original.interest_modal === InterestModal.FlashLoan && (
               <div className=" flex flex-col items-center justify-center md:col-span-4 col-span-12 md:pt-0 pt-4 md:border-t-0 border-t border-tulia_primary w-full">
+                <div className="w-full">
+                  <span>
+                    <Button
+                      type="button"
+                      className="capitalize bg-tulia_primary/50 w-full mt-2"
+                    >
+                      Claim Interest
+                    </Button>
+                  </span>
+                </div>
                 <Dialog>
                   <DialogTrigger type="button" className="w-full">
                     <Button

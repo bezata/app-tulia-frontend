61aa63e0ca0b9552c1220799b91bd49b448edb06
Pufyda <juandecrime@gmail.com>
2024-06-19 03:56:50 +0300

    PeerToPeerTable sorting

--- src/components/PeerToPeerTable/PeerToPeerTable.tsx
+++ src/components/PeerToPeerTable/PeerToPeerTable.tsx
@@ -5,6 +5,8 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getSortedRowModel,
+  SortingState,
 } from '@tanstack/react-table';
 
 import {
@@ -18,7 +20,7 @@ import {
 import { Button } from '../ui/button';
 import { useAccount } from 'wagmi';
 import { Wallet } from 'lucide-react';
-
+import React from 'react';
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
@@ -28,10 +30,16 @@ export function PeerToPeerTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sort, setSort] = React.useState<SortingState>([]);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSort,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting: sort,
+    },
   });
 
   const account = useAccount();

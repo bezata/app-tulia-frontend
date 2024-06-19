f51f721e7c31fdf1e90a0c365b1a8cbc5f70fc95
Pufyda <juandecrime@gmail.com>
2024-06-19 12:31:36 +0300

    MyPoolsTable

--- src/components/MyPoolsTable/columns.tsx
+++ src/components/MyPoolsTable/columns.tsx
@@ -12,6 +12,8 @@ import BorrowViewModal from '../PoolsViewModal/BorrowViewModal/BorrowViewModal';
 import LendViewModal from '../PoolsViewModal/LendViewModal/LendViewModal';
 import { Button } from '../ui/button';
 import { ArrowUpDown } from 'lucide-react';
+import Image from 'next/image';
+import { formatEther } from 'viem';
 
 export enum PoolState {
   Active = 'Active',
@@ -31,38 +33,133 @@ export enum InterestModal {
 export type IPoolsdata = {
   lending_id: string;
   wallet_address: string;
-  coin: string;
+  Token: string;
   amount: number;
-  created_at: string;
   type: number;
-  state: PoolState;
   interest_modal: InterestModal;
+  interestRate: bigint;
+  repaymentPeriod: bigint;
   loan_state: PoolState;
+  numericValue: number | undefined;
+  loanToken: string;
+  borrowToken: string;
 };
 
 export const columns: ColumnDef<IPoolsdata>[] = [
   {
     accessorKey: 'lending_id',
     header: 'Loan ID',
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center justify-center">
+          <div>
+            <span>{row.original.lending_id}</span>
+          </div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: 'wallet_address',
     header: 'Wallet Address',
-  },
-  {
-    accessorKey: 'coin',
-    header: 'Coin',
     cell: ({ row }) => {
       return (
-        <div className="flex items-center gap-2">
-          {row.original.coin === 'ETH' && <EthIcon width={24} height={24} />}
-          {row.original.coin === 'BTC' && <BtcIcon width={24} height={24} />}
-          {row.original.coin === 'TUL' && <BtcIcon width={24} height={24} />}
-          {row.original.coin === 'USDC' && <USDCIcon width={24} height={24} />}
-          {row.original.coin === 'ARB' && <ArbIcon width={24} height={24} />}
-          {row.original.coin === 'DAI' && <DaiIcon width={24} height={24} />}
-          {row.original.coin === 'UNI' && <UniIcon width={24} height={24} />}
-          <span>{row.original.coin}</span>
+        <div className="flex items-center ">
+          <div>
+            <span>{row.original.wallet_address}</span>
+          </div>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'loanToken',
+    header: 'Loan Token',
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center gap-1">
+          {row.original.loanToken === 'ETH' && (
+            <EthIcon width={24} height={24} />
+          )}
+          {row.original.loanToken === 'BTC' && (
+            <BtcIcon width={24} height={24} />
+          )}
+          {row.original.loanToken === 'USDC' && (
+            <USDCIcon width={24} height={24} />
+          )}
+          {row.original.loanToken === 'ARB' && (
+            <ArbIcon width={24} height={24} />
+          )}
+          {row.original.loanToken === 'DAI' && (
+            <DaiIcon width={24} height={24} />
+          )}
+          {row.original.loanToken === 'UNI' && (
+            <UniIcon width={24} height={24} />
+          )}
+          <span>{row.original.loanToken}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'borrowToken',
+    header: 'Borrow Token',
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center gap-1">
+          {row.original.borrowToken === 'ETH' && (
+            <EthIcon width={24} height={24} />
+          )}
+          {row.original.borrowToken === 'BTC' && (
+            <BtcIcon width={24} height={24} />
+          )}
+          {row.original.borrowToken === 'USDC' && (
+            <USDCIcon width={24} height={24} />
+          )}
+          {row.original.borrowToken === 'ARB' && (
+            <ArbIcon width={24} height={24} />
+          )}
+          {row.original.borrowToken === 'DAI' && (
+            <DaiIcon width={24} height={24} />
+          )}
+          {row.original.borrowToken === 'UNI' && (
+            <UniIcon width={24} height={24} />
+          )}
+          <span>{row.original.borrowToken}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'interestRate',
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        className="hover:bg-transparent"
+        onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+      >
+        Interest Rate
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-col gap-2  ">
+          <div className="px-3  ml-9">
+            <span>{`${String(row.original.interestRate)} %`}</span>
+          </div>
+          <div className="flex px-3  ml-4 ">
+            <span className="text-purple-400 flex  px-1 items-center min-w-16 w-16 border text-xs border-white/[0.2] bg-transparent  rounded-sm">
+              <Image
+                src="/logo.png"
+                alt="Logo"
+                width={20}
+                height={20}
+                className="flex"
+              />
+              {`${String(row.original.numericValue)}%`}
+            </span>
+          </div>
         </div>
       );
     },
@@ -82,28 +179,43 @@ export const columns: ColumnDef<IPoolsdata>[] = [
     cell: ({ row }) => {
       return (
         <p className="text-gray-400 text-left ml-4">
-          {row.original.amount} {row.original.coin}
+          {formatEther(BigInt(row.original.amount))} {row.original.Token}
         </p>
       );
     },
   },
   {
-    accessorKey: 'created_at',
+    accessorKey: 'repayment_period',
     header: ({ column }) => (
       <Button
         variant="ghost"
         className="hover:bg-transparent"
         onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
       >
-        Created Date
+        Repayment Period
         <ArrowUpDown className="ml-2 h-4 w-4" />
       </Button>
     ),
     cell: ({ row }) => {
+      const repaymentPeriodInDays =
+        Number(row.original.repaymentPeriod) / 86400;
+      let displayRepaymentPeriod = repaymentPeriodInDays.toFixed(2);
+
+      if (displayRepaymentPeriod.endsWith('.00')) {
+        displayRepaymentPeriod = repaymentPeriodInDays.toFixed(0);
+      } else if (displayRepaymentPeriod.endsWith('0')) {
+        displayRepaymentPeriod = repaymentPeriodInDays.toFixed(1);
+      }
+
       return (
-        <p className="text-gray-400 text-left ml-4">
-          {new Date(row.original.created_at).toDateString()}
-        </p>
+        <div className="flex items-center ml-12">
+          <div>
+            <span>
+              {displayRepaymentPeriod} day
+              {displayRepaymentPeriod !== '1' ? 's' : ''}
+            </span>
+          </div>
+        </div>
       );
     },
   },

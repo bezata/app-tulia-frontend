f343583a8bf314aba640db846997787a87048614
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2024-07-02 13:50:56 +0300

    Fee integrated

--- src/app/tulialend/page.tsx
+++ src/app/tulialend/page.tsx
@@ -4,15 +4,7 @@ import { CardDemo } from '@/components/TuliaComingSoon/TuliaComing';
 const page = () => {
   return (
     <div className="container mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12">
-      {/* Coming soon */}
-      <div className="text-center">
-        <h1 className="text-4xl font-extrabold mt-8 text-primary sm:text-5xl">
-          Tulia Decentralized Personalized Lending Protocol
-        </h1>
-        <p className="text-xl mt-4">Coming soon for Mainnet</p>
-        <p className="text-xl">Stay tuned!</p>
-        {/* description */} <CardDemo />
-      </div>
+      <CardDemo />
     </div>
   );
 };
--- src/components/TuliaComingSoon/TuliaComing.tsx
+++ src/components/TuliaComingSoon/TuliaComing.tsx
@@ -205,7 +205,7 @@ export const CardSkeletonContainer = ({
   return (
     <div
       className={cn(
-        'h-[15rem] md:h-[20rem] rounded-xl z-40',
+        'h-[18rem] md:h-[22rem] rounded-xl z-40',
         className,
         showGradient &&
           ' [mask-image:radial-gradient(50%_50%_at_50%_50%,white_0%,transparent_100%)]'

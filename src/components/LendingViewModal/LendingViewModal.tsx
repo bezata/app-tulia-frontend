f0df2516e7a19f8c644062d782bd146a51346d4c
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2024-07-18 02:30:47 +0300

    Quick Fix

--- src/components/LendingViewModal/LendingViewModal.tsx
+++ src/components/LendingViewModal/LendingViewModal.tsx
@@ -220,32 +220,43 @@ const LendingViewModal = ({ row }: ILendingViewModalProps) => {
                 wrapLongLines
                 showLineNumbers
                 text={`
-        // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+        Flash Loan Pool Address = ${row.original.pool}
+       // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 import "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
 
-contract MockFlashLoanBorrower is IERC3156FlashBorrower {
+contract TuliaFlashLoanBorrower is IERC3156FlashBorrower {
     using SafeERC20 for IERC20;
 
     IERC3156FlashLender public lender;
     bytes32 public constant CALLBACK_SUCCESS = keccak256("ERC3156FlashBorrower.onFlashLoan");
 
-    // Constructor to set the lender
+    /// @notice Sets the lender address on deployment
+    /// @param _lender The address of the flash loan lender contract
     constructor(address _lender) {
         lender = IERC3156FlashLender(_lender);
     }
 
-    // Function to initiate a flash loan
+    /// @notice Initiates a flash loan
+    /// @param token The address of the ERC20 token to borrow
+    /// @param amount The amount of tokens to borrow
     function initiateFlashLoan(address token, uint256 amount) external {
         // Initiating a flash loan
         lender.flashLoan(this, token, amount, bytes("Arbitrary data"));
     }
 
-    // Callback function that runs after receiving the flash loan
+    /// @notice Callback function after receiving the flash loan
+    /// @dev Must be called by the lender contract only
+    /// @param initiator The address that initiated the loan
+    /// @param token The ERC20 token address
+    /// @param amount The borrowed amount
+    /// @param fee The fee amount for the flash loan
+    /// @param data Arbitrary data passed in flash loan
+    /// @return The success callback signal
     function onFlashLoan(
         address initiator,
         address token,
@@ -256,17 +267,18 @@ contract MockFlashLoanBorrower is IERC3156FlashBorrower {
         require(msg.sender == address(lender), "Only lender can call this function");
         require(initiator == address(this), "Untrusted loan initiator");
 
-        // Here you can place your arbitrary logic with the borrowed amount
-        // For instance, conducting an arbitrage, performing swaps, or other operations
+        // Custom logic with the borrowed amount
+        // Example: arbitrage, swaps, or other operations
 
-        // Total amount that needs to be repaid to the flash loan provider
+        // Total amount that needs to be repaid
         uint256 totalRepayment = amount + fee;
         IERC20(token).safeIncreaseAllowance(address(lender), totalRepayment);
         IERC20(token).safeTransferFrom(address(this), address(lender), totalRepayment);
 
-        return CALLBACK_SUCCESS;  // Signalling that the flash loan was handled successfully
+        return CALLBACK_SUCCESS; // Signaling that the flash loan was handled successfully
     }
 }
+
 
           `}
               />
